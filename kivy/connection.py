ce1efc3645e2b673a8b1c1e6f22b9970206adbc9
ChaosArnhug <HE201932@students.ephec.be>
2021-12-02 20:19:52 +0100

    Massive update kivy
    -delete de login.kv, login.py, register.kv, register.py
    - ajout de connection.kv, connection.py
    
    ==> Changement:
    Regroupement des applications login et register sous une même application.
    Pas de changement pour les traitement, la MAJ est purement pour facilité les choses

---
+++ kivy/connection.py
@@ -0,0 +1,53 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivymd.uix.snackbar import Snackbar
+
+
+class Connection(MDApp):
+    def build(self):
+        return Builder.load_file("connection.kv")
+
+    def login(self):
+        pseudo = self.root.ids.l_pseudo.text
+        password = self.root.ids.l_password.text
+        if pseudo == "" or password == "":
+            Snackbar(
+                text="[color=#ffffff]Invalid pseudo or password ! [/color]",
+                font_size="20dp",
+                bg_color=[118/255, 106/255, 221/255, 1],
+                snackbar_animation_dir="Top"
+
+            ).open()
+            return
+        print(pseudo, password)
+        # Reset field
+        self.root.ids.l_pseudo.text = ""
+        self.root.ids.l_password.text = ""
+
+        # Appel de la fonction de traitement ici
+
+    def register(self):
+        pseudo = self.root.ids.r_pseudo.text
+        password = self.root.ids.r_password.text
+        password_confirm = self.root.ids.r_password_confirm.text
+        email = self.root.ids.r_email.text
+
+        if pseudo == "" or password == "" or password_confirm == "" or email == "":
+            Snackbar(
+                text="[color=#ffffff]All field must be completed ! [/color]",
+                font_size="20dp",
+                bg_color=[118/255, 106/255, 221/255, 1],
+                snackbar_animation_dir="Top"
+
+            ).open()
+            return
+        print(pseudo, password, password_confirm, email)
+        self.root.ids.r_pseudo.text = ""
+        self.root.ids.r_password.text = ""
+        self.root.ids.r_password_confirm.text = ""
+        self.root.ids.r_email.text = ""
+
+        # Appel de la fonction de traitement ici
+
+
+Connection().run()
