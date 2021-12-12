63a20ef3dc46123b953b00d3e6548e1ed282ddbd
Abderrachid BELLAALI <91373823+Rachiid94@users.noreply.github.com>
2021-12-12 16:49:55 +0100

    modif ok

--- User_Verification.py
+++ User_Verification.py
@@ -63,7 +63,7 @@ def is_valid_pseudo(pseudo):
     :post: return bool: True if the pseudo contains a character, a digit or _ - + @ and
         its size is between 4 and 25 otherwise False
     """
-    if not re.match(r'\b[A-Za-z0-9._+-@]{4,25}\b', pseudo):
+    if not re.match(r'\b[A-Za-z0-9._+-@]{5,25}\b', pseudo):
         return False, "Le Pseudo ne respect pas la norme !"
     else:
         return True, "pseudo ok"
@@ -154,7 +154,7 @@ def register_verify(user_name, email, age, password, confimation_password):
         return True, "L'utilisateur a été créée"
 
     else:
-        return "probleme validation"
+        return "les champ ne respect pas la norme !"
 
 
 def login_verify(user_name, password):
@@ -171,8 +171,10 @@ def login_verify(user_name, password):
 
 def update_verify(current_user, new_user_name, new_email, new_first_name, new_last_name, new_password,
                   new_password_confim, new_security_question, new_security_answer):
-    if is_valid_pseudo(new_user_name) and is_valide_email(new_email) and is_same_password(new_password,
-                                                                                          new_password_confim):
+
+    if is_valid_pseudo(new_user_name)[0] and is_valide_email(new_email)[0] and \
+            is_same_password(new_password, new_password_confim)[0]:
+
         user_test = Users(user_name=current_user, email="", password="")
         user_test.update(new_user_name, new_email, new_first_name, new_last_name, new_password, new_security_question,
                          new_security_answer)
@@ -186,23 +188,13 @@ def delete_user(user_name):
 
 
 if __name__ == '__main__':
-    # register_verify("Abderrachid", "bellaalirachid@gmail.com", 53, "abdel", "abdel")
-    # user1.create()
-    # user1.delete_all_users()
+    user_opera1 = UsersOperations()
 
     # print(login_verify("rachid1080", "abdel1234"))
 
     # register_verify("rachid1080", "bellaalirachid@gmail.com", 36, "abdel1234", "abdel1234")
 
-    user_opera1 = UsersOperations()
     # delete_user("ChaosArnhug")
-    print(user_opera1.get_all_users())
 
-    # print(update_verify("rachid1080", "tarek1070", "tarek@oliphant.com", "Tarek", "Chaabi", "tarek123", "tarek123", "date de naissance ?", "21-04-1998"))
-
-    # print(user1.is_user_in_bdd())
-    # il_est_dans_la_db = user1.is_user_in_bdd()[0]
-    # print(il_est_dans_la_db)
-
-    # list_users()
-    # print(user1.is_exist_user_name())
+    print(update_verify("rachid1080", "", "tarek@oliphant.com", "Tarek", "Chaabi", "tarek123", "tarek123", "date de naissance ?", "21-04-1998"))
+    # print(user_opera1.get_all_users())
