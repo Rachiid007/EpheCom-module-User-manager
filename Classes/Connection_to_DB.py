368a9a763e50876590f62849265ca19f82b06ef7
ChaosArnhug <HE201932@students.ephec.be>
2021-12-17 14:24:56 +0100

    Modification pour respecter la pep8 et enlever les erreurs de typo

--- Classes/Connection_to_DB.py
+++ Classes/Connection_to_DB.py
@@ -5,15 +5,18 @@ class MongoConnector:
 
     def __init__(self):
         """
-        Crée une nouvelle instance MongoClient
+        Create a new instance MongoClient
+
         PRE:
-        POST: crée la connection avec la BDD via un certificat, un uri, un compte dev
+        POST: Create a connection with the database through a certificate, a uri and a developer account
         """
-        certificat_path = "2TM1-G2.pem"
-        uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
+
+        certificate_path = "2TM1-G2.pem"
+        uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external" \
+              "&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
         client = MongoClient(uri,
                              tls=True,
-                             tlsCertificateKeyFile=certificat_path)
+                             tlsCertificateKeyFile=certificate_path)
         self.db = client['ephecom']
 
     def __enter__(self):
