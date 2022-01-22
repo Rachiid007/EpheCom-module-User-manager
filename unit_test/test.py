764669589d7a8b95f67ec562e9cba0e13860f989
Rachiid94 <abderrachid.bellaali@gmail.com>
2022-01-22 17:05:57 +0100

    automa3

--- unit_test/test.py
+++ unit_test/test.py
@@ -1,9 +1,10 @@
+from tests_rachid import *
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from tests_rachid import *
+
 
 import unittest
 
