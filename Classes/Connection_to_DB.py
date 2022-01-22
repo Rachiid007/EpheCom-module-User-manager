a0b33ea1a5277c14a0dcbaba2eac05a098d9d653
Rachiid94 <abderrachid.bellaali@gmail.com>
2022-01-22 16:26:16 +0100

    automa3

--- Classes/Connection_to_DB.py
+++ Classes/Connection_to_DB.py
@@ -11,7 +11,7 @@ class MongoConnector:
         POST: Create a connection with the database through a certificate, a uri and a developer account
         """
 
-        certificate_path = "../Classes/2TM1-G2.pem"
+        certificate_path = "./2TM1-G2.pem"
         uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external" \
               "&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
         client = MongoClient(uri,
