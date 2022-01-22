38ef3f26f6dc49fd0b2bf14938497b4daddaebcd
Rachiid94 <abderrachid.bellaali@gmail.com>
2022-01-22 18:31:57 +0100

    ok

--- unit_test/test.py
+++ unit_test/test.py
@@ -1,9 +1,11 @@
-
-from tests_rachid import *
 import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from tests_rachid import *
+
+import unittest
 
 if __name__ == "__main__":
     unittest.main()
