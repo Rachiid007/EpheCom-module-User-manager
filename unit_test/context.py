725a475f8bdf66e294f7fc0cc91bbccdb3146f17
Abderrachid BELLAALI <91373823+Rachiid94@users.noreply.github.com>
2022-01-22 14:57:18 +0100

    Delete context.py

--- unit_test/context.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from Classes.Connection_to_DB import *
-from Classes.Permission import *
-from Classes.Roles import *
-from Classes.Users import *
