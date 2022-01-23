616b25051ad2dd50d3e102ef2d18298ff2430285
Rachiid94 <abderrachid.bellaali@gmail.com>
2022-01-23 11:36:18 +0100

    automa4

--- Classes/Connection_to_DB.py
+++ Classes/Connection_to_DB.py
@@ -14,9 +14,16 @@ class MongoConnector:
         """
 
         # indexroot = sys.path[-2].split("/").index("Projet_Dev2")  # "../Classes/2TM1-G2.pem"
-        root = "/".join(sys.path[0].split("/")[:])
 
-        certificate_path = os.path.join(root, "Classes/2TM1-G2.pem")
+        certificate_path = ""
+
+        if sys.platform == "linux":
+            root = "/".join(sys.path[0].split("/")[:])
+            certificate_path = os.path.join(root, "Classes/2TM1-G2.pem")
+
+        if sys.platform == "win32":
+            root = "\\".join(sys.path[0].split("\\")[:])
+            certificate_path = os.path.join(root, "Classes\\2TM1-G2.pem")
 
         uri = "mongodb+srv://cluster0.5i6qo.gcp.mongodb.net/ephecom?authSource=%24external" \
               "&authMechanism=MONGODB-X509&retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE"
