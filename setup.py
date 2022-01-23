d4e38b42fc84ca30aa263e4011805ae87bfc373f
Rachiid94 <abderrachid.bellaali@gmail.com>
2022-01-23 03:19:35 +0100

    automa3

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="example-package-YOUR-USERNAME-HERE",
+    version="0.0.1",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "Classes"},
+    packages=setuptools.find_packages(where="Classes"),
+    python_requires=">=3.6",
+)
--- unit_test/tests_rachid.py
+++ unit_test/tests_rachid.py
@@ -8,6 +8,21 @@ class RachidTest(unittest.TestCase):
         self.Uop = UsersOperations()
         self.viU = ValidationsInfosUsers()
 
+    def test_is_not_exist_pseudo(self):
+        with self.assertRaises(PseudoNotValid):
+            # existe dans la DB
+            self.Uop.is_not_exist_pseudo("")
+
+        with self.assertRaises(PseudoNotValid):
+            # existe dans la DB
+            self.Uop.is_not_exist_pseudo("rachiid007")
+
+        with self.assertRaises(PseudoNotValid):
+            # existe dans la DB
+            self.Uop.is_not_exist_pseudo("totototo")
+
+        self.assertEqual(self.Uop.is_not_exist_pseudo("chaos"), True, " 'chaos' -> n'existe pas dans la DB")
+
     def test_is_age_min_13_yeas(self):
         with self.assertRaises(AgeNotValid):
             # vide !
@@ -24,6 +39,34 @@ class RachidTest(unittest.TestCase):
         with self.assertRaises(AgeNotValid):
             # no OK
             self.viU.is_age_min_13_years("2025-11-5")
+
+    def test_register_verify(self):
+        with self.assertRaises(PseudoNotValid):
+            # les champs sont vide
+            register_verify("", "", "", "", "", "", "")
+
+        with self.assertRaises(PseudoNotValid):
+            # l'utilisateur existe déjà
+            register_verify("totototo", "totototo@ephec.com", "2002-04-21", "abdel1234", "abdel1234",
+                            "C'est qui toto ?", "c'est toto")
+
+        with self.assertRaises(EmailNotValid):
+            # L'email existe déjà
+            register_verify("userno007", "toto@ephec.be", "1998-04-21", "abdel1234", "abdel1234", "c'est qui Toto",
+                            "c'est Toto")
+
+        with self.assertRaises(PasswordNotValid):
+            # Le MDP ne respect pas la norme
+            register_verify("userno007", "hdbyhdb@ephec.com", "1998-04-21", "aaa", "aaa", "c'est qui Toto",
+                            "c'est Toto")
+
+        with self.assertRaises(PasswordsNotSame):
+            # Les 2 MDP sont pas identique
+            register_verify("userno007", "hdbyhdb@ephec.com", "1998-04-21", "abdel123", "rachid123", "c'est qui Toto",
+                            "c'est Toto")
+
+        self.assertTrue(register_verify("rachid007", "rachid@ephec.com", "1998-04-21", "abdel1234", "abdel1234",
+                                        "C quoi mon meilleur langage de progra ?", "Python "), "Tu t'es inscrit")
 
 
 if __name__ == '__main__':
